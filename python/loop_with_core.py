b79051f5c21d468ac872da0261baca5b567f409b
fgolf <frank.golf@gmail.com>
2021-04-30 21:11:02 -0700

    bleh

--- python/loop_with_core.py
+++ python/loop_with_core.py
@@ -4,7 +4,9 @@ import ROOT as r
 import os
 
 ch = r.TChain("Events")
-ch.Add("root://redirector.t2.ucsd.edu///store/user/namin/nanoaod/TTJets_TuneCP5_13TeV-madgraphMLM-pythia8__RunIIAutumn18NanoAODv5-Nano1June2019_102X_upgrade2018_realistic_v19-v1/8E0C8306-DC0D-0548-BA7C-D0698140DF28.root");
+#ch.Add("root://redirector.t2.ucsd.edu///store/user/namin/nanoaod/TTJets_TuneCP5_13TeV-madgraphMLM-pythia8__RunIIAutumn18NanoAODv5-Nano1June2019_102X_upgrade2018_realistic_v19-v1/8E0C8306-DC0D-0548-BA7C-D0698140DF28.root");
+#ch.Add("root://redirector.t2.ucsd.edu///store/mc/RunIISummer16NanoAODv7/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_Nano02Apr2020_102X_mcRun2_asymptotic_v8_ext1-v1/260000/6393A45C-BFE8-6948-87A7-5D478B9ECA57.root");
+ch.Add('file:///home/users/fgolf/forYash/dyjets_RunIISummer16NanoAOD.root')
 
 r.gSystem.Load('../NanoCORE/NANO_CORE.so')
 for include in ["Nano.cc", "SSSelections.cc", "MetSelections.cc"]:
